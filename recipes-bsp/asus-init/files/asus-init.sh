1e4ff66aa2857ab24cbb6c7eb97fe0ad89c7f549
Rock Shen <rock_shen@asus.com>
2023-05-16 07:59:57 +0800

    set act-led trigger function in asus-init
    Change-Id: Ib8fdbe90de2d031d5b782f707435e86ff5066c42
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- recipes-bsp/asus-init/files/asus-init.sh
+++ recipes-bsp/asus-init/files/asus-init.sh
@@ -2,9 +2,24 @@
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 
+# set act-led trigger function
+do_set_led_trigger()
+{
+	cmdline=$(cat /proc/cmdline)
+	storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+	if [ $storage -gt 0 ]; then
+		#emmc
+		echo mmc0 > /sys/class/leds/act-led/trigger
+	else
+		#sdcard
+		echo mmc1 > /sys/class/leds/act-led/trigger
+	fi
+}
+
 case "$1" in
 	start)
 		echo -n "Starting ASUS init"
+		do_set_led_trigger
 		echo "."
 		;;
 	stop)
