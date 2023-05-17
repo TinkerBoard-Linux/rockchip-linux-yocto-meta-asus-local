671c1e1157c2f2a7d3bec7ee83e32ba7dad51d9a
Rock Shen <rock_shen@asus.com>
2023-05-17 14:29:56 +0800

    Test_tool: add test_usb_file_transfer.sh
    Change-Id: I491f4edfd7ec9cfa93ae29cbbcfdb4bdf1b0d699
    Signed-off-by: Rock Shen <rock_shen@asus.com>

---
+++ asus-overlay/overlay-debug/home/root/Test_tool/test_usb_file_transfer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version=1.0
+
+info_view()
+{
+	echo "************************************************"
+	echo
+	echo                "USB File Transfer Test v_$version"
+	echo
+	echo "************************************************"
+	echo
+	echo
+}
+
+info_view
+usbdev=$(lsblk|grep sda1)
+if [ -z "$usbdev" ];then
+	echo "No USB Storage found"
+	exit
+else
+	sudo mount /dev/sda1 /mnt/usb_storage
+
+	echo "Start Write Test"
+	sync
+	echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null 2>&1
+	sudo dd if=/dev/zero of=/mnt/usb_storage/tmpfile bs=256M count=15 conv=fdatasync
+
+	echo "Start Read Test"
+	sync
+	echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null 2>&1
+	sudo dd if=/mnt/usb_storage/tmpfile of=/dev/null bs=256M count=15
+	sudo rm /mnt/usb_storage/tmpfile
+	sudo umount /dev/sda1
+fi
