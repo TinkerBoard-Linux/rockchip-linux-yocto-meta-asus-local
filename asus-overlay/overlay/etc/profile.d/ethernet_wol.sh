d10b196414d3532876de28d85c87ae6220ac9b85
yupei_lin <YuPei_Lin@asus.com>
2023-08-21 10:52:48 +0800

    Ethernet: remove "exit" to avoid device boot fail
    Change-Id: I1ef634e826a5ac2f8d0fba94f7a70a8d05f7751e

--- asus-overlay/overlay/etc/profile.d/ethernet_wol.sh
+++ asus-overlay/overlay/etc/profile.d/ethernet_wol.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Enable WOL for ethernet : g=on, d=off
+
 eth0=$(sudo ifconfig eth0 | grep "eth0")
-if [ "$eth0" == "" ]
-then
+if [ "$eth0" == "" ]; then
 	echo "eth0 not exist"
-	exit
+else
+	/usr/sbin/ethtool -s eth0 wol g
 fi
 
-# Enable WOL for ethernet : g=on, d=off
-/usr/sbin/ethtool -s eth0 wol g
