6cdabe10cde105ba75aa58f2df37703f7eae34f2
RyanLY_Wang <RyanLY_Wang@asus.com>
2024-03-22 17:48:54 +0800

    FotaClient: update script and config file
    Change-Id: I880621affdc08c9aa0c7b27c971e059a76f324b0

--- asus-overlay/overlay/sbin/start-fotaclient.sh
+++ asus-overlay/overlay/sbin/start-fotaclient.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-echo "fotaclient start" > /dev/kmsg
+echo "[FotaClient]: Start" > /dev/kmsg
 
-#while true; do
 alived=$(ps | grep -v 'grep' | grep 'ota_daemon.pyc')
 if [ "${alived}" != "" ]; then
-    echo "[fotaclient]: fota is running" > /dev/kmsg
-    #break
+    echo "[FotaClient]: Client is running" > /dev/kmsg
 else
-    echo "[fotaclient]: fota is not running, restart" > /dev/kmsg
+    echo "[FotaClient]: Client is not running, restart" > /dev/kmsg
     /usr/bin/python3 /usr/share/FotaClient/ota_daemon.pyc &
 fi
-    #sleep 5
-#done
-
-echo "fotaclient end" > /dev/kmsg
