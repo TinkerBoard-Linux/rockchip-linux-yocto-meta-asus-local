c6445a450175d41f744a49de5113947086a9fca0
yupei_lin <YuPei_Lin@asus.com>
2024-07-11 17:09:19 +0800

    Ethernet: using project model to distinguish tinker-3/3n
    1. Tinker-3: single lan
    2. Tinker-3n: dual lan
    
    Change-Id: Iea6b9e5c14d9528ec35397b3d76b2ad209b011b0

--- asus-overlay/overlay/etc/profile.d/ethernet_wol.sh
+++ asus-overlay/overlay/etc/profile.d/ethernet_wol.sh
@@ -2,6 +2,8 @@
 
 # Enable WOL for ethernet : g=on, d=off
 
+project_model=$(tr -d '\0' < /proc/device-tree/model)
+
 eth0=$(sudo ifconfig eth0 | grep "eth0")
 if [ "$eth0" == "" ]; then
 	echo "eth0 not exist"
@@ -9,10 +11,12 @@ else
 	/usr/sbin/ethtool -s eth0 wol g
 fi
 
-eth1=$(sudo ifconfig eth1 | grep "eth1")
-if [ "$eth1" == "" ]; then
-	echo "eth1 not exist"
-else
-	/usr/sbin/ethtool -s eth1 wol g
+if [ "$project_model" != "ASUS Tinker Board 3" ]; then
+	eth1=$(sudo ifconfig eth1 | grep "eth1")
+	if [ "$eth1" == "" ]; then
+		echo "eth1 not exist"
+	else
+		/usr/sbin/ethtool -s eth1 wol g
+	fi
 fi
 
