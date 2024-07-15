1fcc0be8b2e79212f36e37836ed2879894067be1
kengyen_chen <kengyen_chen@asus.com>
2024-07-15 11:44:40 +0800

    Audio: Use the board name to determine which jack status to read for yocto
    RK356X TB3N Jack status : /sys/class/extcon/extcon4/cable.1/state
    RK3566 TB3 Jack status : /sys/class/extcon/extcon3/cable.1/state
    
    Change-Id: I1e590943e253427150aaead7718f00672a7f5df7

--- asus-overlay/overlay/usr/bin/drm-hotplug.sh
+++ asus-overlay/overlay/usr/bin/drm-hotplug.sh
@@ -3,15 +3,22 @@
 
 hdmi_status=$(cat /sys/class/drm/card0-HDMI-A-1/status)
 dp_status=$(cat /sys/class/drm/card0-DP-1/status)
-jack_status=$(cat /sys/class/extcon/extcon4/cable.1/state)
+board_name=$(cat /proc/device-tree/boardinfo/model | awk '{gsub("\0","")} 1')
+
+if [ $board_name = "rk3566" ];
+then
+    jack_status=$(cat /sys/class/extcon/extcon3/cable.1/state)
+else
+    jack_status=$(cat /sys/class/extcon/extcon4/cable.1/state)
+fi
 
 # Config audio output devices when HDMI hot-plug
 if [ $hdmi_status = "connected" ] && [ $jack_status = 0 ];
 then
-	echo "Plug-in HDMI, set default sound card to HDMI"
+	echo "Plug-in HDMI, set default sound card to HDMI" > /dev/kmsg
 	/etc/pulse/switch_sound_device.sh "alsa_output.platform-hdmi-sound.stereo-fallback"
 else
-	echo "Plug-out HDMI, set default sound card to RK809"
+	echo "Plug-out HDMI, set default sound card to RK809" > /dev/kmsg
 	/etc/pulse/switch_sound_device.sh "alsa_output.platform-rk809-sound.HiFi__hw_rockchiprk809__sink"
 fi
 
